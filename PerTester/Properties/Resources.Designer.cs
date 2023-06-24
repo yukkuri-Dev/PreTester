1b8e7103258c2c677e53b08f83c59bed6742e4ed
yukkuri-Dev <76156149+yukkuri-Dev@users.noreply.github.com>
2023-06-24 22:25:56 +0900

    作者情報を消しました(2/1)

--- PerTester/Properties/Resources.Designer.cs
+++ PerTester/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PerTester.Properties {
+namespace PreTester.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PerTester.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PerTester.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PreTester.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
